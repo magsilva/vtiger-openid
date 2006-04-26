30017c07931c030e531e471df9fad7ab44646a88
msilva <msilva@378214df-5912-0410-8aa2-a19dbde6e33a>
2006-04-26 20:03:38 +0000

    Imported the vTiger 4.2.3.

---
+++ modules/squirrelmail-1.4.4/plugins/mail_fetch/index.php
@@ -0,0 +1,17 @@
+<?php
+
+   /**
+    **  index.php -- Displays the main frameset
+    **
+    **  Copyright (c) 1999-2005 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Redirects to the login page.
+    **
+    **  $Id: index.php,v 1.1 2005/09/02 11:02:04 indigoleopard Exp $
+    **/
+
+   header("Location:../../src/login.php\n\n");
+   exit();
+
+?>
